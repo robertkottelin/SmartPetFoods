da441be3ef474149bc173e58f95fd2c0e32d6df5
robertkottelin <robert.kottelin@gmail.com>
2023-03-07 14:17:08 +0100

    updates

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,18 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-//
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-// ----------------------------------------------------------------------
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App />);
-
-// If you want to enable client cache, register instead.
-serviceWorker.unregister();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
