7923339b8df1b3bb7c23c63651b1af3cff8ffd78
robertkottelin <robert.kottelin@gmail.com>
2023-03-11 17:59:00 +0100

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,31 +1,65 @@
-import React from 'react'
-import Inputstuff from './Inputstuff'
+import React, { useState } from 'react';
+import Inputstuff from './Inputstuff';
 import productsData from './products.json';
 
 export default function App() {
+  const [sortBy, setSortBy] = useState('name');
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  const sortedProducts = productsData.sort((a, b) => {
+    const isDesc = sortOrder === 'desc';
+    const [first, second] = isDesc ? [b, a] : [a, b];
+    switch (sortBy) {
+      case 'name':
+        return first.name.localeCompare(second.name);
+      case 'price':
+        return first.price - second.price;
+      case 'weight':
+        return first.weight - second.weight;
+      case 'kcal':
+        return first.kcal - second.kcal;
+      case 'price/kcal':
+        return (first.price / first.kcal) - (second.price / second.kcal);
+      default:
+        return 0;
+    }
+  });
+
+  const handleHeaderClick = (columnName) => {
+    if (sortBy === columnName) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(columnName);
+      setSortOrder('asc');
+    }
+  };
+
   return (
     <>
       <Inputstuff />
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Weight</th>
-            <th>kcal</th>
+            <th onClick={() => handleHeaderClick('name')}>Name</th>
+            <th onClick={() => handleHeaderClick('price')}>Price</th>
+            <th onClick={() => handleHeaderClick('weight')}>Weight</th>
+            <th onClick={() => handleHeaderClick('kcal')}>kcal</th>
+            <th onClick={() => handleHeaderClick('price/kcal')}>Price/kcal</th>
           </tr>
         </thead>
         <tbody>
-          {productsData.map((item, index) => (
+          {sortedProducts.map((item, index) => (
             <tr key={index}>
               <td>{item.name}</td>
               <td>{item.price}</td>
               <td>{item.weight}</td>
               <td>{item.kcal}</td>
+              <td>{(item.price / (item.weight * 100 * item.kcal / 100)).toFixed(2)}</td>
+              <td>{(item.weight * 100 * item.kcal / 100).toFixed(2)}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </>
-  )
+  );
 }
